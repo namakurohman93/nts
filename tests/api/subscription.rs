5c9eaac7e1b5edcff1cd0dd4441c9a1f9d02dac3
didadadida93 <didadadida93@gmail.com>
2023-06-23 18:26:00 +0700

    pending_confirmation for new subscriber

--- tests/api/subscription.rs
+++ tests/api/subscription.rs
@@ -15,14 +15,32 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     let response = app.post_subscriptions(body.into()).await;
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+    assert_eq!(200, response.status().as_u16());
+
+    app.teardown_database().await;
+}
+
+#[tokio::test]
+async fn subscribe_persists_the_new_subscriber() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions")
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
 
-    assert_eq!(200, response.status().as_u16());
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
+    assert_eq!(saved.status, "pending_confirmation");
 
     app.teardown_database().await;
 }
