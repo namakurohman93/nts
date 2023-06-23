beee9069c8b60939fdad50700f79677266fb21d1
didadadida93 <didadadida93@gmail.com>
2023-06-23 22:31:53 +0700

    Finalizing chapter 8

--- tests/api/subscription.rs
+++ tests/api/subscription.rs
@@ -128,3 +128,20 @@ async fn subscribe_sends_a_confirmation_email_with_a_link() {
 
     app.teardown_database().await;
 }
+
+#[tokio::test]
+async fn subscribe_fails_if_there_is_a_fatal_database_error() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    sqlx::query!("ALTER TABLE subscriptions DROP COLUMN email;")
+        .execute(&app.db_pool)
+        .await
+        .unwrap();
+
+    let response = app.post_subscriptions(body.into()).await;
+
+    assert_eq!(response.status().as_u16(), 500);
+
+    app.teardown_database().await;
+}
